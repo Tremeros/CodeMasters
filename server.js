12887137bd34049f0114ebd6cd181ace1a82c265
Paweł Tylczyński <pawel.tylczynski@gmail.com>
2020-05-06 21:34:25 +0200

    Prepare for deployment

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDB();
 
 
 // app.use(express.json({extended: false}));
-
+app.use(express.json());
 
 app.use(bodyParser.json({ type: '*/*'}));
 
@@ -19,18 +19,19 @@ app.use(bodyParser.json({ type: '*/*'}));
 
 app.use('/api/message', require('./routes/api/message'));
 
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
     app.use(express.static('client/build'));
-
+  
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
-}
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 
 
 const PORT = process.env.PORT || 5000;
-const server = http.createServer(app);
 
 
-server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
