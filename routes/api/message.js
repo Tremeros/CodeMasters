8f47d37e65992b22d2f33ee1f28ab7ea58359e91
Paweł Tylczyński <pawel.tylczynski@gmail.com>
2020-05-07 13:23:26 +0200

    Email

--- routes/api/message.js
+++ routes/api/message.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {check, validationResult} = require('express-validator/check');
 const Message = require('../../models/message');
+const nodemailer = require('nodemailer');
 
 router.get('/', async (req, res) => {
     res.send('Mesage');
@@ -29,6 +30,31 @@ router.post('/', [
           });     
           
           await newMessage.save();
+
+          const transport = nodemailer.createTransport({
+            service: 'gmail',
+             auth: {
+                user: 'pawel.tylczynski@gmail.com',
+                pass: 'w1niasz'
+    }
+        });
+
+        const mailOptions = {
+            from: email,
+            to: "pawel.tylczynski@gmail.com",
+            subject: "Nowa wiadomość",
+            text: message,
+            html: `<b>${message} ${email}</b>`,
+        };
+        
+
+        await transport.sendMail(mailOptions).then((result)=>{
+            console.log(result);
+            console.log(email);
+            console.log(message);
+        }).catch((error)=>{
+            console.log(error);
+        })
 
           res.json(newMessage);
     } catch (err) {
